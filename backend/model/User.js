dd229f3ffa3bdd37a8caeff831bf29d9627f6147
Chisamuel651 <chijoe0306@gmail.com>
2024-08-02 15:16:46 +0100

    backend configuration

---
+++ backend/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+// compiled schema to form a model meaning that we're going to use User to create instances
+module.exports = mongoose.model('User', userSchema);
